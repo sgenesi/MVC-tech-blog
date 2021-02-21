1ea00ac54a6b8202dee20a5d57d9c7578dca224e
Staci Genesi <sgenesi@hotmail.com>
2021-02-21 16:05:30 -0700

    create user model

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { MOdel, DataTypes } = require('sequlize');
+const sequlize = require('../config/connection');
+
+// create User model
+class User extends Model { }
+
+// define table columns and configuration
+User.init(
+    {
+        // defines an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // defines a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // defines a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
