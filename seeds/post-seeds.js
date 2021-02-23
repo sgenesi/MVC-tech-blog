da1e5a8e32676570c74713796f833fb638f67f4f
Staci Genesi <sgenesi@hotmail.com>
2021-02-22 19:17:54 -0700

    update handlebars code

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,88 +1,34 @@
 const { Post } = require('../models');
 
-const postdata = [
+const postData = [
     {
-        title: 'Donec posuere metus vitae ipsum.',
-        user_id: 10
+        title: "Taskmaster goes public!",
+        post_content: "Taskinator is a task-tracker app that will allow you to organize your personal to-do list items by clicking and dragging them into categories like To Do, In Progress, and Completed.",
+        user_id: 3
     },
     {
-        title: 'Morbi non quam nec dui luctus rutrum.',
-        user_id: 8
-    },
-    {
-        title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+        title: "Zoo Keepr reaches 1 million subscribers!",
+        post_content: "The local zoo has received funding to build a new online catalog, and they've asked to create a web server for a front-end application they’re developing, called Zoo Keepr. This site’s data will be stored on the server. This will allow animal enthusiasts to access the data from different locations and browsers without needing to download it to their device",
         user_id: 1
     },
     {
-        title: 'Nunc purus.',
-        user_id: 4
+        title: "Work Day Scheduler tool now available!",
+        post_content: "A simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery. Use Moment.js library to work with date and time",
+        user_id: 2
+
     },
     {
-        title: 'Pellentesque eget nunc.',
-        user_id: 7
-    },
-    {
-        title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-        user_id: 4
-    },
-    {
-        title: 'In hac habitasse platea dictumst.',
-        user_id: 1
-    },
-    {
-        title: 'Morbi non quam nec dui luctus rutrum.',
-        user_id: 1
-    },
-    {
-        title: 'Duis ac nibh.',
-        user_id: 9
-    },
-    {
-        title: 'Curabitur at ipsum ac tellus semper interdum.',
+        title: "Tech Blog has been released!",
+        post_content: "A CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well. This site will be built from scratch and deployed to Heroku. The app will follow MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication",
         user_id: 5
     },
     {
-        title: 'In hac habitasse platea dictumst.',
-        user_id: 3
-    },
-    {
-        title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
-        user_id: 10
-    },
-    {
-        title: 'Donec dapibus.',
-        user_id: 8
-    },
-    {
-        title: 'Nulla tellus.',
-        user_id: 3
-    },
-    {
-        title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
-        user_id: 3
-    },
-    {
-        title: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
-        user_id: 7
-    },
-    {
-        title: 'In hac habitasse platea dictumst.',
-        user_id: 6
-    },
-    {
-        title: 'Etiam justo.',
+        title: "Just Tech News goes public!",
+        post_content: "Just Tech News—a tech news website where users can post, upvote, and comment on links to news articles. Use Sequelize, an object-relational mapping (ORM) library, to simplify your MySQL queries, add password hashing so that users can create secure passwords, and connect your application to JawsDB, a MySQL add-on for Heroku",
         user_id: 4
-    },
-    {
-        title: 'Nulla ut erat id mauris vulputate elementum.',
-        user_id: 6
-    },
-    {
-        title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-        user_id: 7
     }
-];
+]
 
-const seedPosts = () => Post.bulkCreate(postdata);
+const seedPosts = () => Post.bulkCreate(postData);
 
 module.exports = seedPosts;
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,59 +1,38 @@
-const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User } = require('../models');
 
-const userdata = [
+const userData = [
     {
-        username: 'alesmonde0',
-        email: 'nwestnedge0@cbc.ca',
-        password: 'password123'
+        username: "ross_gellar",
+        email: "ross_gellar@gmail.com",
+        password: "password1"
     },
     {
-        username: 'jwilloughway1',
-        email: 'rmebes1@sogou.com',
-        password: 'password123'
+        username: "joey_tribbiani",
+        email: "joey_tribbiani@gmail.com",
+        password: "password2"
     },
     {
-        username: 'iboddam2',
-        email: 'cstoneman2@last.fm',
-        password: 'password123'
+        username: "chandler_bing",
+        email: "chandler_bing@gmail.com",
+        password: "password3"
     },
     {
-        username: 'dstanmer3',
-        email: 'ihellier3@goo.ne.jp',
-        password: 'password123'
+        username: "rachel_green",
+        email: "rachel_green@gmail.com",
+        password: "password4"
     },
     {
-        username: 'djiri4',
-        email: 'gmidgley4@weather.com',
-        password: 'password123'
+        username: "monica_gellar",
+        email: "monica_gellar@gmail.com",
+        password: "password5"
     },
     {
-        username: 'msprague5',
-        email: 'larnout5@imdb.com',
-        password: 'password123'
-    },
-    {
-        username: 'mpergens6',
-        email: 'hnapleton6@feedburner.com',
-        password: 'password123'
-    },
-    {
-        username: 'tpenniell7',
-        email: 'kperigo7@china.com.cn',
-        password: 'password123'
-    },
-    {
-        username: 'msabbins8',
-        email: 'lmongain8@google.ru',
-        password: 'password123'
-    },
-    {
-        username: 'jmacarthur9',
-        email: 'bsteen9@epa.gov',
-        password: 'password123'
+        username: "phoebe_buffay",
+        email: "phoebe_buffaya@gmail.com",
+        password: "password6"
     }
-];
+]
 
-const seedUsers = () => User.bulkCreate(userdata, { individualHooks: true });
+const seedUsers = () => User.bulkCreate(userData);
 
 module.exports = seedUsers;
